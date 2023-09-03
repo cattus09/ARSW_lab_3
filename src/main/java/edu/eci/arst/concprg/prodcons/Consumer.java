839a7b7d9a8c1c71fcd498d0b37c2901a4d31cbb
NicolasCastro9 <tankiandown@gmail.com>
2023-09-03 12:03:38 -0500

    punto1.3 cambio

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -24,7 +24,7 @@ public class Consumer extends Thread{
     public void run() {
         while (true) {
                 try {
-                    Thread.sleep(10000);
+                    Thread.sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
